ec661f014012cbbccbb1f0d091cb8314d25b79b3
Christian <christianpereiradasilva2006@gmail.com>
2026-03-08 15:15:41 -0300

    Login and Signup page created

--- PersonalFinanceAngular/src/app/app.routes.ts
+++ PersonalFinanceAngular/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './components/login-page/login-page';
+import { SignupPage } from './components/signup-page/signup-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginPage
+    },
+    {
+        path: 'signup',
+        component: SignupPage
+    }
+];
---
+++ PersonalFinanceAngular/src/app/components/signup-page/signup-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'app-signup-page',
+  imports: [NgOptimizedImage],
+  templateUrl: './signup-page.html',
+  styleUrl: './signup-page.css',
+})
+export class SignupPage {}
